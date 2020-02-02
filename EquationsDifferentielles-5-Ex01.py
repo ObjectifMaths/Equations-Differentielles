73c4df7624ceabeaf78e3fb6d5f59f5ad3c5811e
ObjectifMaths <objectifmaths@tutanota.com>
2020-02-02 05:37:04 +0100

    ajout d'une courbe

--- EquationsDifferentielles-5-Ex01.py
+++ EquationsDifferentielles-5-Ex01.py
@@ -3,32 +3,33 @@ import matplotlib.pyplot as plt
 import scipy.integrate as spig
 
 f=lambda y,t: 2/t*y-1
-y0=1.2
 
-# à droite de t0
-T=np.linspace(1,2)
-Z=spig.odeint(f,y0,T)
-plt.plot(T,Z,'g-')
-plt.plot(T[0],y0,'go')
-xx,XX=min(T), max(T)
+CI=[(1,1.2,'g'), (1.5,1,'r')]
+xx, XX=-2,2
 
-# à gauche de t0
-T=np.linspace(1,-2)
-Z=spig.odeint(f,y0,T)
-plt.plot(T,Z,'g-')
+for (t0,y0,col) in CI:
+    T=np.linspace(t0,XX) # à droite de t0
+    Z=spig.odeint(f,y0,T)
+    plt.plot(T,Z,col+'-')
+    plt.plot(T[0],y0,col+'o', label="y({})={}".format(t0,y0))
+    
+    T=np.linspace(t0,xx) # à gauche de t0
+    Z=spig.odeint(f,y0,T)
+    plt.plot(T,Z,col+'-')
+    
 
 
-plt.title(r"$ ty'-2y=-t \quad y({})={} $".format(T[0],y0),fontsize=20)
-plt.ylim(-1,2.5) # limite l'axe des ordonnées
+plt.title("$ ty'-2y=-t$",fontsize=20)
+
 
 # trace les axes
-xx,XX= min(min(T),xx), max(max(T),XX)
-yy,YY=plt.ylim()
+yy,YY=plt.ylim(-1,2.5) # limite l'axe des ordonnées
 plt.plot([0,0],[yy,YY],'k-', [xx,XX],[0,0],'k-')
 plt.axes().set_aspect('equal')
 
 plt.grid()
+plt.legend()
 plt.savefig("EquationsDifferentielles-5-Ex01.pdf")
-plt.show()
+#plt.show()
 
 
